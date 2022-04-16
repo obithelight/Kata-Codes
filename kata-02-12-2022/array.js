5cdf4485bfe2f6a1ebcb9e2583eb3c86646bd478
Kenneth Onu <kennetho.onu@gmail.com>
2022-04-16 18:55:51 +0800

    First element of an array not consecutive

--- kata-02-12-2022/array.js
+++ kata-02-12-2022/array.js
@@ -9,3 +9,5 @@ If the whole array is consecutive then return null2.
 
 The array will always have at least 2 elements1 and all elements will be numbers. The numbers will also all be unique and in ascending order. The numbers could be positive or negative and the first non-consecutive could be either too!
 */
+
+//Solution 1
