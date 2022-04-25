b7b5f5d7cd78756f47bc16d542c975f34cac6a40
Kenneth Onu <kennetho.onu@gmail.com>
2022-04-25 16:21:31 +0800

    Total cost for a rental car

--- kata-04-12-2022/rental-cost.js
+++ kata-04-12-2022/rental-cost.js
@@ -5,3 +5,8 @@ Write a code that gives out the total amount for different days(d).
 */
 
 //Solution 1
+function rentalCarCost(d) {
+  if(d < 3) return d * 40
+  if(d >= 3 && d < 7) return (d * 40) - 20
+  if(d >= 7) return (d * 40) - 50
+}
