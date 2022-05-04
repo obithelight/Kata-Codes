5ef3989b4aa5d2718e72808d17cf99248a234c95
Kenneth Onu <kennetho.onu@gmail.com>
2022-05-04 15:21:35 +0800

    Find and remove the minimum value in an array

--- kata-30-11-2022/minimum.js
+++ kata-30-11-2022/minimum.js
@@ -19,3 +19,12 @@ function removeSmallest(numbers) {
 }
 
 //Solution
+function removeSmallest(numbers) {
+   let array = [];
+   const index = numbers.indexOf(Math.min(...numbers))
+      for(let i = 0; i < numbers.length; i++){
+         if ( i !== index){
+           array.push(numbers[i])}   
+      }
+ return array;
+} 
