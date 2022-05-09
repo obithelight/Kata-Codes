310b303e7a41bddec0f9fdd31e5b31d95436b47c
Kenneth Onu <kennetho.onu@gmail.com>
2022-05-09 09:11:36 +0800

    Distance between the first & last pillar

--- kata-04-12-2022/pillars.js
+++ kata-04-12-2022/pillars.js
@@ -7,3 +7,4 @@ width of the pillar (10 - 50 centimeters).
 Calculate the distance between the first and the last pillar in centimeters (without the width of the first and last pillar).
 */
 
+//Solution 1
