e11684e1ef6992f0d6db00753c2dd1bc9bed5957
Kenneth Onu <kennetho.onu@gmail.com>
2022-05-09 09:12:01 +0800

    Distance between the first & last pillar

--- kata-04-12-2022/pillars.js
+++ kata-04-12-2022/pillars.js
@@ -8,3 +8,10 @@ Calculate the distance between the first and the last pillar in centimeters (wit
 */
 
 //Solution 1
+function pillars(numPill, dist, width) {
+  if(numPill > 1) {
+  return (numPill - 2) * width + (numPill - 1) * dist * 100;
+  } else {
+    return 0;
+  }
+}
