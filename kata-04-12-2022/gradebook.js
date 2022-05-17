145704b4fd3261fd58cd7c4048e1dc55fd70472f
Kenneth Onu <kennetho.onu@gmail.com>
2022-05-17 12:18:53 +0800

    Find the average of 3 scores

--- kata-04-12-2022/gradebook.js
+++ kata-04-12-2022/gradebook.js
@@ -7,7 +7,7 @@ Numerical Score	Letter Grade
 80 <= score < 90	'B'
 70 <= score < 80	'C'
 60 <= score < 70	'D'
-0 <= score < 60	'F'
+0 <= score < 60		'F'
 Tested values are all between 0 and 100. Theres is no need to check for negative values or values greater than 100.
 */
 
