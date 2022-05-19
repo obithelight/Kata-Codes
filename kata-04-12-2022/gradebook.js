9ea3ba23cfdb45ec4b235a9b232a5a0756c98702
Kenneth Onu <kennetho.onu@gmail.com>
2022-05-19 12:39:10 +0800

    Find the average of 3 scores

--- kata-04-12-2022/gradebook.js
+++ kata-04-12-2022/gradebook.js
@@ -28,3 +28,16 @@ function getGrade (s1, s2, s3) {
 }
 
 //Solution 2
+function getGrade (score1, score2, score3) { 
+  let average = (score1 + score2 + score3) / 3
+  	
+    if(average >= 90 && average <= 100) return "A"
+  	
+    else if(average >= 80 && average < 90) return "B"
+  	
+    else if(average >= 70 && average < 80) return "C"
+    
+    else if(average >= 60 && average < 70) return "D"
+  	
+    else if(average >= 0 && average < 60) return "F" 
+}
