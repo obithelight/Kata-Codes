3e1bdab2e97e5db9f1c58c423fefd4ea0997a071
Kenneth Onu <kennetho.onu@gmail.com>
2022-06-11 14:03:40 +0800

    Number of days in a month

--- kata-30-11-2022/switch.js
+++ kata-30-11-2022/switch.js
@@ -14,3 +14,10 @@ Tip: Using default for most of the cases can reduce your work.
 */
 
 //Solution 1
+const howManydays = (month) =>{
+  switch (month){
+    case 2: return 28
+    case 4: case 6: case 9: case 11: return 30
+    default: return 31
+  }
+}
