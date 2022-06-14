371e3acb4067d8ec11d0b9cb2a33c0ee5c056966
Kenneth Onu <kennetho.onu@gmail.com>
2022-06-14 14:09:43 +0800

    Number of days in a month

--- kata-30-11-2022/switch.js
+++ kata-30-11-2022/switch.js
@@ -38,3 +38,20 @@ function howManydays(month){
 }
 
 //Solution 3
+function howManydays(month){
+  let days = "";
+  switch (month){
+    case 2:
+      days = 28
+      break;
+    case 4:
+    case 6:
+    case 9:
+    case 11:
+      days = 30
+      break;
+    default:
+      days = 31
+  }
+  return days;
+}
