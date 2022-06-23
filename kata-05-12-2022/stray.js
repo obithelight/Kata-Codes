b5e774c4dfbcb3099aab2e3701565f436f822052
Kenneth Onu <kennetho.onu@gmail.com>
2022-06-24 06:41:05 +0800

    Find the different element in an array

--- kata-05-12-2022/stray.js
+++ kata-05-12-2022/stray.js
@@ -9,3 +9,13 @@ Examples
 */
 
 //Solution 1
+function stray(numbers) {
+ let strayChar = numbers[0]
+ if(strayChar !== numbers[1] && strayChar !== numbers[2])
+   return strayChar
+  
+  for(let i = 1; i < numbers.length; i++){
+    if(strayChar !== numbers[i])
+      return numbers[i]
+  }
+}
