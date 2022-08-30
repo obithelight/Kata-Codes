6bdcbb50971e56e1bdbca1a885b91f57395ac7c5
Kenneth Onu <kennetho.onu@gmail.com>
2022-08-31 07:41:09 +0800

    Count the number of petals

--- kata-23-11-2022/petals.js
+++ kata-23-11-2022/petals.js
@@ -34,3 +34,15 @@ function howMuchILoveYou(nbPetals) {
 }
 
 //Solution 2
+const phrases = [
+    'I love you',
+    'a little',
+    'a lot',
+    'passionately',
+    'madly',
+    'not at all',
+]
+
+function howMuchILoveYou(n) {
+     return phrases[(n - 1) % phrases.length] 
+}
