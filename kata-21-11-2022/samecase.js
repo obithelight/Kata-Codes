33feb1ae8d67ed4ddb2be05c916a4612ed387825
Kenneth Onu <kennetho.onu@gmail.com>
2022-09-16 14:35:01 +0800

    Check if the given Characters are of the same case

--- kata-21-11-2022/samecase.js
+++ kata-21-11-2022/samecase.js
@@ -28,3 +28,14 @@ function sameCase(a, b){
 }
 
 //Solution 2
+function sameCase(a, b){
+    const alpha = 'abcdefghijklmnopqrstuvwxyz'
+    if(!alpha.includes(a.toLowerCase()) || !alpha.includes(b.toLowerCase())) return -1
+    
+    const checkCase = x => {
+      if(x === x.toLowerCase()) return 'lower'
+      if(x === x.toUpperCase()) return 'upper'
+    }
+  
+    return checkCase(a) === checkCase(b) ? 1 : 0
+  }
