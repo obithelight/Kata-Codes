2bc6ea724c30f09f8e6d6e258796fb6acc6ac4b5
Kenneth Onu <kennetho.onu@gmail.com>
2022-09-19 15:48:26 +0800

    Find the nearest square number of a positive integer

--- kata-21-11-2022/square.js
+++ kata-21-11-2022/square.js
@@ -1,3 +1,11 @@
 /*
 Your task is to find the nearest square number, nearest_sq(n), of a positive integer n.
+
+describe("Basic tests",() => {
+  Test.assertEquals(nearestSq(1),1)
+  Test.assertEquals(nearestSq(2),1)
+  Test.assertEquals(nearestSq(10),9)
+  Test.assertEquals(nearestSq(111),121)
+  Test.assertEquals(nearestSq(9999),10000)
+});
 */
