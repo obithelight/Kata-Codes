b0de2affa575dd5635f06f64e313e2db9ac4e962
Kenneth Onu <kennetho.onu@gmail.com>
2022-10-03 17:43:56 +0800

    Multiplication table

--- kata-22-11-2022/multiplication.js
+++ kata-22-11-2022/multiplication.js
@@ -46,3 +46,12 @@ const multiTable = (n) => {
 }
 
 //Solution 4
+const multiTable = (number) => {
+  let table = '';
+  
+  for(let i = 1; i <= 10; i++) {
+    table += `${i} * ${number} = ${i * number}${i < 10 ? '\n' : ''}`; 
+  }
+
+  return table;
+}
