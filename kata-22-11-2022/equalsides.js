35a40d833744592930dae4f732d3cd14e6cf2dff
Kenneth Onu <kennetho.onu@gmail.com>
2022-10-10 19:26:34 +0800

    Find equal sides of an array

--- kata-22-11-2022/equalsides.js
+++ kata-22-11-2022/equalsides.js
@@ -30,3 +30,15 @@ If you are given an array with multiple answers, return the lowest correct index
 */
 
 //Solution 1
+function findEvenIndex(arr)
+{
+  var left = 0, right = arr.reduce(function(pv, cv) { return pv + cv; }, 0);
+  for(var i = 0; i < arr.length; i++) {
+      if(i > 0) left += arr[i-1];
+      right -= arr[i];
+      
+      if(left == right) return i;
+  }
+  
+  return -1;
+}
