44e1c15941ecec801b5518d85e344e2759f4a7df
Kenneth Onu <kennetho.onu@gmail.com>
2022-10-11 20:02:32 +0800

    Find equal sides of an array

--- kata-22-11-2022/equalsides.js
+++ kata-22-11-2022/equalsides.js
@@ -44,3 +44,17 @@ function findEvenIndex(arr)
 }
 
 //Solution 2
+function findEvenIndex(arr)
+{
+  var left = 0, right = arr.reduce(function(pv, cv) { return pv + cv; }, 0);
+  for(var i = 0; i < arr.length; i++) {
+      if(i > 0) left += arr[i-1];
+      right -= arr[i];
+      
+      if(left == right) return i;
+  }
+  
+  return -1;
+}
+
+//Solution 3
