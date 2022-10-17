1008f46b00e9c4604eac8b4641fa10de7f2398d8
Kenneth Onu <kennetho.onu@gmail.com>
2022-10-17 11:17:43 +0800

    Feast of beasts

--- kata-20-11-2022/feast.js
+++ kata-20-11-2022/feast.js
@@ -10,3 +10,5 @@ Assume that beast and dish are always lowercase strings, and that each has at le
 function feast(beast, dish) {
 return dish[0] === beast[0] && dish[dish.length - 1] === beast[beast.length - 1]
 }
+
+//Solution 2
