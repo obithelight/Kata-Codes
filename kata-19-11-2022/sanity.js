9996fec494a6a69a03c1f4ebca156d4ef2517a8c
Kenneth Onu <kennetho.onu@gmail.com>
2022-10-20 22:16:56 +0800

    Return to sanity

--- kata-19-11-2022/sanity.js
+++ kata-19-11-2022/sanity.js
@@ -2,3 +2,10 @@
 This function should return an object, but it's not doing what's intended. What's wrong?
 */
 
+function mystery() {
+  let results = {
+    sanity: 'Hello'
+  };
+  return results;
+}
+
