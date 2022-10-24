ec47311ad900902e69d88d2161c03620ac28d67c
Kenneth Onu <kennetho.onu@gmail.com>
2022-10-24 10:34:12 +0800

    Summation of numbers from 1 to that number

---
+++ kata-20-11-2022/summation.js
@@ -0,0 +1,8 @@
+/*
+Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0.
+
+For example:
+
+summation(2) -> 3
+1 + 2
+*/
