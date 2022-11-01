bf2c5969fc5e4358e744db233921b291433d610e
Kenneth Onu <kennetho.onu@gmail.com>
2022-11-01 10:08:27 +0800

    Find the Area & Perimeter of a Polygon

--- kata-20-11-2022/polygon.js
+++ kata-20-11-2022/polygon.js
@@ -7,3 +7,5 @@ Example(Input1, Input2 --> Output):
 3, 3 --> 9
 Note: for the purposes of this kata you will assume that it is a square if its length and width are equal, otherwise it is a rectangle.
 */
+
+//Solution 1
