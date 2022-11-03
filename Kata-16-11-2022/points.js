05b5cb1b9fb69780644fe33d09487abd3650d3f3
Kenneth Onu <kennetho.onu@gmail.com>
2022-11-03 09:43:45 +0800

    Total points

--- Kata-16-11-2022/points.js
+++ Kata-16-11-2022/points.js
@@ -39,3 +39,8 @@ function points(games) {
 const points = games => games.reduce((sum, [x, , y]) => sum + (x > y ? 3 : x == y), 0)
 
 //Solution 3
+const points = a => a.reduce((r, e) => {
+  const x = e[0];
+  const y = e[2];
+  return r + (x > y ? 3 : x < y ? 0 : 1);
+}, 0);
