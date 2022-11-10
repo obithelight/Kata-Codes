beac2641145b771b6b18c29255beaf82ed6f1b39
Kenneth Onu <kennetho.onu@gmail.com>
2022-11-10 20:54:38 +0800

    If..Else Statement/Ternary Operator

--- kata-19-11-2022/if-else.js
+++ kata-19-11-2022/if-else.js
@@ -8,3 +8,13 @@ n >= 10 90
 You can use if..else or ternary operator to complete it.
 */
 
+//Solution 1
+function saleHotdogs(n){
+  if(n < 5){
+    return 100 * n
+  }if((n >= 5) && (n < 10)){
+    return 95 * n
+  }if(n >= 10){
+    return 90 * n
+  }
+}
