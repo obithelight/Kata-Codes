6c517f29c7eb5dff09fda6828d0ff29addb18840
Kenneth Onu <kennetho.onu@gmail.com>
2022-11-18 18:48:27 +0800

    Form a pythagorean triple with 3 integers

---
+++ kata-19-11-2022/pythagorean.js
@@ -0,0 +1,10 @@
+/*
+Given an array of 3 non-negative integers a, b and c, determine if they form a pythagorean triple.
+
+A pythagorean triple is formed when:
+
+c2 = a2 + b2
+where c is the largest value of a, b, c.
+
+For example: a = 3, b = 4, c = 5 forms a pythagorean triple, because 52 = 32 + 42
+*/
