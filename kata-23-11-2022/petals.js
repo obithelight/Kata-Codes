4ee0f334cc35b300689f1caeab598a3d2134e22a
Kenneth Onu <kennetho.onu@gmail.com>
2022-11-23 07:25:03 +0800

    Count the number of petals

--- kata-23-11-2022/petals.js
+++ kata-23-11-2022/petals.js
@@ -15,3 +15,20 @@ Your goal in this kata is to determine which phrase the girls would say at the l
 */
 
 //Solution 1
+function howMuchILoveYou(nbPetals) {
+  switch((nbPetals - 1) % 6){
+      case 1:
+        return "a little"
+      case 2:
+        return "a lot"
+      case 3: 
+        return "passionately"
+      case 4:
+        return "madly"
+      case 5:
+        return "not at all"
+      default: 
+        return "I love you"
+      break;
+  }
+}
