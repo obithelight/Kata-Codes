a7959bcc9da184c18cbe12e424af56e9ebb2df59
Kenneth Onu <kennetho.onu@gmail.com>
2022-11-22 17:23:35 +0800

    Multiplication table

--- kata-22-11-2022/multiplication.js
+++ kata-22-11-2022/multiplication.js
@@ -17,3 +17,10 @@ P. S. You can use \n in string to jump to the next line.
 */
 
 //Solution 1
+function multiTable(number) {
+  let table = '';
+  for(let i = 1; i <= 10; i++){
+    table += `${i} * ${number} = ${i*number}\n`
+  }
+  return table.slice(0, -1)
+}
