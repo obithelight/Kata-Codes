809d7ffb8850efc0dcd4929c378d76766d662af0
Kenneth Onu <kennetho.onu@gmail.com>
2022-12-01 17:11:29 +0800

    Return the quarter of a month

--- kata-02-12-2022/quarter.js
+++ kata-02-12-2022/quarter.js
@@ -4,7 +4,7 @@ Given a month as an integer from 1 to 12, return to which quarter of the year it
 For example: month 2 (February), is part of the first quarter; month 6 (June), is part of the second quarter; and month 11 (November), is part of the fourth quarter.
 */
 
-//Solution 1
+//Solution 1 (SWITCH STATEMENT)
 const quarterOf = month => {
   switch(month){
     case 1: case 2: case 3: return 1 
