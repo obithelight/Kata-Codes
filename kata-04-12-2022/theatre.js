4299e815b0c14b34461cb50219bbf521c40eeb4e
Kenneth Onu <kennetho.onu@gmail.com>
2022-12-04 06:17:25 +0800

    Return the total number of people seating behind you

--- kata-04-12-2022/theatre.js
+++ kata-04-12-2022/theatre.js
@@ -41,3 +41,6 @@ The number of people who sit strictly behind you and in your column or to the le
 */
 
 //Solution 1
+function seatsInTheater(nCols, nRows, col, row) {
+  return ((nCols - col + 1) * (nRows - row))
+}
