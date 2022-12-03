3eaae3ded8419264ff0ba75834cb2cbfc18a8ffb
Kenneth Onu <kennetho.onu@gmail.com>
2022-12-04 06:32:01 +0800

    Remove duplicates from an array

--- kata-02-12-2022/duplicate.js
+++ kata-02-12-2022/duplicate.js
@@ -14,3 +14,14 @@ function distinct(a) {
 const distinct = a => a.filter((item, index) => a.indexOf(item) === index);
 
 //Solution 3
+function distinct(arr) {
+  let result = []; 
+  
+  for(let i = 0; i < arr.length; i++){
+    if(!result.includes(arr[i])){
+      result.push(arr[i]);
+    }
+}
+return result;
+}
+
