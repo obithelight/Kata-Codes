323e073ac0775e96ae7cba03f69b64102bd4f9f5
Kenneth Onu <kennetho.onu@gmail.com>
2022-12-05 06:38:10 +0800

    Find the different element in an array

--- kata-05-12-2022/stray.js
+++ kata-05-12-2022/stray.js
@@ -1,1 +1,9 @@
-/**/
+/*
+Complete the method which accepts such an array, and returns that single different number.
+
+The input array will always be valid! (odd-length >= 3)
+
+Examples
+[1, 1, 2] ==> 2
+[17, 17, 3, 17, 17, 17, 17] ==> 3
+*/
