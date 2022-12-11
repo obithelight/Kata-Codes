68fecbf901c22f5dcef9e517d327695d3a57950c
Kenneth Onu <kennetho.onu@gmail.com>
2022-12-11 09:09:57 +0800

    Convert a binary number to decimal number

--- kata-30-11-2022/decimal.js
+++ kata-30-11-2022/decimal.js
@@ -2,8 +2,9 @@
 Complete the function which converts a binary number (given as a string) to a decimal number.
 */
 
-//Solution
+//Solution 1
 function binToDec(bin){
   return parseInt(bin,2)
 }
 
+//
